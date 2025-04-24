72f13f067be9094b0b3a7abab1024cef82f4a7bd
Ime <Email>
2025-04-24 09:36:19 +0300

    Add project files.

---
+++ FootFetish/Controllers/ProductsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using FootFetish.Data;
+using FootFetish.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace FootFetish.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            var products = _context.Products;
+            return View(products);
+        }
+
+
+        [Authorize(Roles = "Admin")]
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name");
+            return View();
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,Name,Description,Price,ImageUrl,CategoryIds")] Product product)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            _context.Products.Add(product);
+
+            foreach (var catid in product.CategoryIds)
+            {
+                var cat = await _context.Categories.FindAsync(catid);
+                cat.Products.Add(product);
+                _context.Update(cat);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? id)
+        {
+            var product = await _context.Products
+        .FirstOrDefaultAsync(p => p.ProductId == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            List<string> cats = new List<string>();
+
+            foreach(int catid in product.CategoryIds)
+            {
+                var categ = await _context.Categories.FirstOrDefaultAsync(c => c.CategoryId == catid);
+                cats.Add(categ.Name);
+            }
+
+            ViewBag.CategoryNames = cats;
+
+            return View("~/Views/Products/Details.cshtml", product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", product.CategoryIds);
+            return View(product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,Name,Description,Price,ImageUrl,CategoryId")] Product product)
+        {
+            if (id != product.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(product);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ProductExists(product.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.Categories, "CategoryId", "Name", product.CategoryIds);
+            return View(product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            
+            var product = await _context.Products
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            List<string> cats = new List<string>();
+
+            foreach(int catid in product.CategoryIds)
+            {
+                var cat = await _context.Categories.FirstOrDefaultAsync(c => c.CategoryId == catid);
+                cats.Add(cat.Name);
+            }
+            ViewBag.Categories = cats;
+
+            return View(product);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+            if (product != null)
+            {
+                _context.Products.Remove(product);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ProductExists(int id)
+        {
+            return _context.Products.Any(e => e.ProductId == id);
+        }
+
+    }
+}
---
+++ FootFetish/Models/OrderViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace FootFetish.Models
+{
+    public class OrderViewModel
+    {
+        public string PaymentMethod { get; set; }
+        public List<CartItem> CartItems { get; set; }
+        public string Address { get; set; }
+        public string DeliveryMethod { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
+}
